729d0a94a90914f4fedcd46010506c8a2b61c181
Michael Oznobkin <michaeloznobkin@Michaels-MacBook-Pro-2.local>
2018-10-17 09:56:25 +0300

    fix: fte

--- src/__tests__/generateRND.ts
+++ src/__tests__/generateRND.ts
@@ -1,32 +1,28 @@
-import { config } from 'dc-configs'
-import { Eth } from '../Eth'
-import * as Utils from '../utils'
+import { config } from "dc-configs"
+import { Eth } from "../Eth"
+import * as Utils from "../utils"
 
 const {
   gasPrice: price,
   gasLimit: limit,
   web3HttpProviderUrl: httpProviderUrl,
   contracts,
-  privateKey,
+  privateKey
 } = config
 
 const eth = new Eth({
   httpProviderUrl,
   ERC20ContractInfo: contracts.ERC20,
   gasParams: { price, limit },
-  privateKey,
+  privateKey
 })
 
 const test1 = () => {
   eth.initAccount()
   const seed = Utils.makeSeed()
-  const hash = eth.signHash([
-    {t: 'bytes32', v: seed}
-  ])
+  const hash = eth.signHash([{ t: "bytes32", v: seed }])
 
   const rnd = eth.generateRnd([[0, 10], [0, 10], [0, 10]], hash)
-
-  console.log(rnd)
 }
 
 test1()
