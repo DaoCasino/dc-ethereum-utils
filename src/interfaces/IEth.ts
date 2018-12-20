8ee15bf8a1668b1a21e81b49e7a5a8c99f02ff27
Ilya fanyShu <ilyashvy@gmail.com>
2018-12-20 16:13:54 +0300

    fix(dc-wallet): refactor dc-wallet
    shared functions utils and walletFactory
    delivered blockchain types in separate package
    dc-blockchain-types
    refactor getBalances and send token and send currency method

--- src/interfaces/IEth.ts
+++ src/interfaces/IEth.ts
@@ -1,38 +1,39 @@
-import Contract from "web3/eth/contract"
-import { Account as Web3Account } from "web3/eth/accounts"
+import { GasParams } from '@daocasino/dc-blockchain-types'
 import { ContractInfo } from "@daocasino/dc-configs"
+// import Contract from "web3/eth/contract"
+// import { Account as Web3Account } from "web3/eth/accounts"
 
-type SolidityType =
-  | "bytes32"
-  | "address"
-  | "uint"
-  | "uint256"
-  | "bytes"
-  | "bool"
+// type SolidityType =
+//   | "bytes32"
+//   | "address"
+//   | "uint"
+//   | "uint256"
+//   | "bytes"
+//   | "bool"
 
-export interface SolidityTypeValue {
-  t: SolidityType
-  v: string | string[] | number[] | boolean
-}
+// export interface SolidityTypeValue {
+//   t: SolidityType
+//   v: string | string[] | number[] | boolean
+// }
 
-export interface Balance {
-  balance?: number
-  updated?: number
-}
+// export interface Balance {
+//   balance?: number
+//   updated?: number
+// }
 
-export interface LastBalances {
-  bet: Balance
-  eth: Balance
-}
+// export interface LastBalances {
+//   bet: Balance
+//   eth: Balance
+// }
 
-export interface Cache {
-  lastBalances: LastBalances
-}
+// export interface Cache {
+//   lastBalances: LastBalances
+// }
 
-export interface GasParams {
-  price: number
-  limit: number
-}
+// export interface GasParams {
+//   price: number
+//   limit: number
+// }
 
 export interface EthParams {
   walletName: string
@@ -41,56 +42,47 @@ export interface EthParams {
   gasParams: GasParams
 }
 
-export interface ETHInstance {
-  getAccount: () => Web3Account
-  initContract: (abi: any, address: string) => Contract
-  initAccount: (privateKey: string) => void
+// export interface ETHInstance {
+//   getAccount: () => AccountInterface
+//   initContract: (abi: any, address: string) => ContractInterface
+//   initAccount: (privateKey: string) => void
 
-  createAccountForMnemonic: (
-    mneminicSeed: string,
-    indexForCreate: number
-  ) => {
-    address: string,
-    privateKey: string
-  }
+//   saveWallet: (privateKey: string, walletPassword?: string) => void
+//   loadWallet: (walletPassword: string) => void
+//   getWalletAccount: () => any
 
-  saveWallet: (privateKey: string, walletPassword?: string) => void
-  loadWallet: (walletPassword: string) => void
-  getWalletAccount: () => any
+//   signData: (argsToSign: SolidityTypeValue[]) => string
+//   signHash: (hash: string) => string
+//   recover: (hash: string, peerSign: string) => string
 
-  signData: (argsToSign: SolidityTypeValue[]) => string
-  signHash: (hash: string) => string
-  recover: (hash: string, peerSign: string) => string
+//   getBlockNumber: () => Promise<any>
 
-  getBlockNumber: () => Promise<any>
+//   allowance: (spender: string, address: string) => Promise<any>
 
-  allowance: (spender: string, address: string) => Promise<any>
+//   sendTransaction: (
+//     contract: ContractInterface,
+//     methodName: string,
+//     args: any[]
+//   ) => Promise<any>
 
-  sendTransaction: (
-    contract: Contract,
-    methodName: string,
-    args: any[],
-    addressFrom?: string
-  ) => Promise<any>
+//   ERC20ApproveSafe: (
+//     spender: string,
+//     amount: number,
+//     minAmount?: number
+//   ) => Promise<number>
 
-  ERC20ApproveSafe: (
-    spender: string,
-    amount: number,
-    minAmount?: number
-  ) => Promise<number>
+//   getBalances: (address?: string) => Promise<LastBalances>
+//   getEthBalance: (address: string) => Promise<Balance>
+//   getBetBalance: (address: string) => Promise<Balance>
 
-  getBalances: (address?: string) => Promise<LastBalances>
-  getEthBalance: (address: string) => Promise<Balance>
-  getBetBalance: (address: string) => Promise<Balance>
-  
-  sendToken(
-    from: string,
-    to: string,
-    amount: number
-  ): Promise<any>
-  sendBlockchainCurrency(
-    from: string,
-    to: string,
-    amount: number
-  ): Promise<any>
-}
+//   sendToken(
+//     from: string,
+//     to: string,
+//     amount: number
+//   ): Promise<TransactionReceipt>
+//   sendBlockchainCurrency(
+//     from: string,
+//     to: string,
+//     amount: number
+//   ): Promise<TransactionReceipt>
+// }
